d7ab280b290424f8a4e4e17397ebdbcb75c4c4a8
Bryan Buchanan <bryan@resen.co>
2022-01-05 00:29:49 -0800

    asdf

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# rgb
- 
+## rgb
+
+1. Make sure `source.json` is the file you want to convert.
+1. Make sure you have node installed.
+1. From terminal, run `npm run convert`.
+1. The result will be printed in terminal.
---
+++ index.js
@@ -0,0 +1,20 @@
+const fs = require('fs')
+
+let json = fs.readFileSync('source.json')
+let data = JSON.parse(json)
+
+const formattedData = {
+	red_ssf: [],
+	green_ssf: [],
+	blue_ssf: [],
+}
+
+for (const [key, set] of Object.entries(data.spectral_data.data.main)) {
+
+	formattedData.red_ssf.push(set[0])
+	formattedData.blue_ssf.push(set[1])
+	formattedData.green_ssf.push(set[2])
+
+}
+
+console.log({formattedData})
